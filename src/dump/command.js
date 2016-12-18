7247801d9b3923de7a92c847055f830afc67990d
Stefano Verna <s.verna@cantierecreativo.net>
2016-12-18 01:19:12 +0100

    --quiet mode

--- src/dump/command.js
+++ src/dump/command.js
@@ -7,75 +7,83 @@ import SiteClient from '../site/SiteClient';
 import detectSsg from './detectSsg';
 import dump from './dump';
 import SiteChangeWatcher from './SiteChangeWatcher';
+import requireToken from './requireToken';
 
 export default function (options) {
   const configFile = path.resolve(options['--config'] || 'dato.config.js');
-  const token = options['--token'] || process.env.DATO_API_TOKEN;
+  const tokenOption = options['--token'] || process.env.DATO_API_TOKEN;
   const watch = options['--watch'];
-  const verbose = options['--verbose'];
+  const quiet = options['--quiet'];
 
-  if (!token) {
-    process.stdout.write(
-      'Missing API token: use the --token option or set an DATO_API_TOKEN environment variable!\n'
+  let tokenPromise;
+
+  if (tokenOption) {
+    tokenPromise = Promise.resolve(tokenOption);
+  } else {
+    tokenPromise = requireToken();
+  }
+
+  return tokenPromise.then((token) => {
+    try {
+      fs.accessSync(configFile);
+    } catch (e) {
+      process.stderr.write(`Missing config file ${configFile}\n`);
+      process.exit(1);
+    }
+
+    const client = new SiteClient(
+      token,
+      {
+        'X-Reason': 'dump',
+        'X-SSG': detectSsg(process.cwd()),
+      }
     );
-    process.exit(1);
-  }
 
-  try {
-    fs.accessSync(configFile);
-  } catch (e) {
-    process.stdout.write(`Missing config file ${configFile}\n`);
-    process.exit(1);
-  }
+    function exec(prefix = '') {
+      let text = 'Fetching content from DatoCMS';
 
-  const client = new SiteClient(
-    token,
-    {
-      'X-Reason': 'dump',
-      'X-SSG': detectSsg(process.cwd()),
-    }
-  );
+      if (prefix) {
+        text = `${prefix}! ${text}`;
+      }
 
-  function exec(prefix = '') {
-    let text = 'Fetching content from DatoCMS';
+      const spinner = ora(text).start();
 
-    if (prefix) {
-      text = `${prefix}! ${text}`;
-    }
-
-    const spinner = ora(text).start();
-
-    return dump(configFile, client)
-      .then((operations) => {
-        spinner.succeed();
-        if (verbose) {
-          process.stdout.write('\n');
-          operations.forEach(operation => process.stdout.write(`* ${operation}\n`));
-          process.stdout.write('\n');
-        }
-      })
-      .catch((e) => {
-        spinner.fail();
-        process.stderr.write(new PrettyError().render(e));
-      });
-  }
-
-  if (watch) {
-    return exec()
-      .then(() => client.site.find())
-      .then((site) => {
-        const watcher = new SiteChangeWatcher(site.id);
-        watcher.connect(exec.bind(null, 'Detected site data change'));
-
-        chokidar.watch(configFile)
-          .on('change', exec.bind(null, 'Detected change to config file'));
-
-        process.on('SIGINT', () => {
-          watcher.disconnect();
-          process.exit();
+      return dump(configFile, client)
+        .then((operations) => {
+          spinner.succeed();
+          if (!quiet) {
+            process.stdout.write('\n');
+            operations.forEach(operation => process.stdout.write(`* ${operation}\n`));
+            process.stdout.write('\n');
+          }
+        })
+        .catch((e) => {
+          spinner.fail();
+          process.stderr.write(new PrettyError().render(e));
         });
-      });
-  }
+    }
 
-  return exec();
+    if (watch) {
+      return exec()
+        .then(() => client.site.find())
+        .then((site) => {
+          const watcher = new SiteChangeWatcher(site.id);
+          watcher.connect(exec.bind(null, 'Detected site data change'));
+
+          chokidar.watch(configFile)
+            .on('change', exec.bind(null, 'Detected change to config file'));
+
+          process.on('SIGINT', () => {
+            watcher.disconnect();
+            process.exit();
+          });
+        });
+    }
+
+    return exec();
+  })
+  .catch((e) => {
+    process.stderr.write(new PrettyError().render(e));
+    process.exit(1);
+  });
 }
---
+++ src/dump/requireToken.js
@@ -0,0 +1,31 @@
+import readline from 'readline';
+import fs from 'fs';
+import denodeify from 'denodeify';
+
+const fsAppendFile = denodeify(fs.appendFile);
+
+export default function () {
+  return new Promise((resolve, reject) => {
+    process.stdout.write('Site token is not specified! Please paste your DatoCMS site read-only API token.\n');
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question('> ', (token) => {
+      rl.close();
+
+      if (token) {
+        resolve(token);
+      }
+
+      reject();
+    });
+  })
+  .then((token) => {
+    return fsAppendFile('.env', `DATO_API_TOKEN=${token}`)
+      .then(() => process.stdout.write('\nToken added to .env file.\n\n'))
+      .then(() => token);
+  });
+}
