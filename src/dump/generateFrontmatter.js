b50be0515a73051bae9c0177f842b5c753477d09
Stephen Bau <bauhouse@gmail.com>
2017-06-05 17:24:19 -0700

    Allow omitting front matter when generating Markdown posts

--- src/dump/generateFrontmatter.js
+++ src/dump/generateFrontmatter.js
@@ -1,18 +1,25 @@
 import serializeData from './serializeData';
 
 export default function generateFrontmatter(format, data) {
-  switch (format) {
-    case 'yaml':
-    case 'yml':
-      return `---\n${serializeData(format, data)}\n---\n\n`;
+  if (data) {
+    switch (format) {
+      case 'yaml':
+      case 'yml':
+        return `---\n${serializeData(format, data)}\n---\n\n`;
 
-    case 'toml':
-      return `+++\n${serializeData(format, data)}\n+++\n\n`;
+      case 'toml':
+        return `+++\n${serializeData(format, data)}\n+++\n\n`;
 
-    case 'json':
-      return `${serializeData(format, data)}\n\n`;
+      case 'json':
+        return `${serializeData(format, data)}\n\n`;
 
-    default:
-      throw new Error(`Unsupported format: ${format}`);
+      case 'md':
+        return '';
+
+      default:
+        throw new Error(`Unsupported format: ${format}`);
+    }
+  } else {
+    return '';
   }
 }
