c692406efcf933df94f9169ddf621aa753e642b0
Matteo Giaccone <matteo@turnintocoders.it>
2019-02-08 14:45:03 +0100

    Fix how we paginate against WP APIs

--- src/wpImport/utils/allPages.js
+++ src/wpImport/utils/allPages.js
@@ -2,21 +2,22 @@
 
 const { spin, progress } = require('../utils/progress');
 
-const allPages = async (label, requestPromise, tick = null, page = 0) => {
+const allPages = async (label, requestPromise, tick = null, page = 1) => {
   const response = await (
     tick
       ? tick('', requestPromise)
       : spin(label, requestPromise)
   );
 
-  if (!response._paging || !response._paging.next) {
+  if (!response._paging || response._paging.totalPages <= page) {
     return response;
   }
 
+  const nextPage = response._paging.next.page(page + 1);
   return response.concat(
     await allPages(
       label,
-      response._paging.next,
+      nextPage,
       tick || progress(label, response._paging.totalPages - 1),
       page + 1,
     ),
