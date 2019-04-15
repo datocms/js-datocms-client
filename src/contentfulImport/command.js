ad35a946c392c942a2c411f3bf3b65c84d2071ab
ullmark <ullmark@gmail.com>
2019-04-15 15:39:14 +0200

    fixed lint issues

--- src/contentfulImport/command.js
+++ src/contentfulImport/command.js
@@ -55,7 +55,7 @@ export default async (contentfulToken, contentfulSpaceId, datoCmsToken, skipCont
     await publishRecords({
       contentfulData,
       contentfulRecordMap,
-      datoClient
+      datoClient,
     });
   }
 
