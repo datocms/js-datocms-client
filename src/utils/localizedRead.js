3ffecbe242c94f2865c5be6569ee672bf440be1f
Matteo Giaccone <matteo@turnintocoders.it>
2019-09-19 18:06:20 +0200

    Prevent empty hash from being localized

--- src/utils/localizedRead.js
+++ src/utils/localizedRead.js
@@ -1,11 +1,11 @@
 export default function localizedRead(entity, key, localized, i18n) {
-  if (localized) {
-    const hash = entity[key];
+  const hash = entity[key];
+  if (localized && hash) {
     const fallbacks = i18n.fallbacks || {};
     const locales = [i18n.locale].concat(fallbacks[i18n.locale] || []);
     const localeWithValue = locales.find(locale => hash[locale]);
     return localeWithValue ? hash[localeWithValue] : null;
   }
 
-  return entity[key];
+  return hash;
 }
