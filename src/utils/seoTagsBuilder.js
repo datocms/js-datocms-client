5f67a4569230f50eb3c54cc4a0a4905f4c10deee
Matteo Giaccone <matteo@turnintocoders.it>
2019-12-03 14:51:39 +0100

    Fix fallback image retrieval in SEO field

--- src/utils/seoTagsBuilder.js
+++ src/utils/seoTagsBuilder.js
@@ -20,20 +20,16 @@ function seoAttributeWithFallback(
   const seoField =
     itemEntity && itemEntity.itemType.fields.find(f => f.fieldType === 'seo');
 
-  const itemSeoValue =
-    seoField &&
-    localizedRead(
-      itemEntity,
-      camelize(seoField.apiKey),
-      seoField.localized,
-      i18n,
-    ) &&
-    localizedRead(
-      itemEntity,
-      camelize(seoField.apiKey),
-      seoField.localized,
-      i18n,
-    )[attribute];
+  let itemSeoValue = null;
+  if (seoField) {
+    itemSeoValue = itemEntity[camelize(seoField.apiKey)];
+
+    if ( attribute === 'image' && itemSeoValue && itemSeoValue.image ) {
+      itemSeoValue = itemSeoValue.image.upload ? itemSeoValue.image.upload.id : null;
+    } else {
+      itemSeoValue = itemSeoValue ? itemSeoValue[attribute] : null;
+    }
+  }
 
   const multiLocaleSite = site.locales.length > 1;
 
@@ -54,13 +50,7 @@ export const builders = {
 
     const title = seoAttributeWithFallback(
       'title',
-      titleField &&
-        localizedRead(
-          itemEntity,
-          camelize(titleField.apiKey),
-          titleField.localized,
-          i18n,
-        ),
+      titleField && itemEntity[camelize(titleField.apiKey)],
       itemEntity,
       entitiesRepo,
       i18n,
@@ -199,14 +189,8 @@ export const builders = {
       itemEntity &&
       itemEntity.itemType.fields
         .filter(f => f.fieldType === 'file')
-        .map(field =>
-          localizedRead(
-            itemEntity,
-            camelize(field.apiKey),
-            field.localized,
-            i18n,
-          ),
-        )
+        .map(field => itemEntity[camelize(field.apiKey)])
+        .map(image => image && image.upload ? image.upload.id : null)
         .filter(id => !!id)
         .map(id => entitiesRepo.findEntity('upload', id))
         .find(
@@ -218,11 +202,11 @@ export const builders = {
             image.height >= 200,
         );
 
-    const itemImageId = itemImage && itemImage.id;
+    const fallbackImageId = itemImage && itemImage.id;
 
     const imageId = seoAttributeWithFallback(
       'image',
-      itemImageId,
+      fallbackImageId,
       itemEntity,
       entitiesRepo,
       i18n,
