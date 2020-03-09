8512cfd7179957a869fce5f4a2033ab1a8cc49ce
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-03-09 20:10:33 +0530

    Add loadSchema and watchSchema

--- src/local/Loader.js
+++ src/local/Loader.js
@@ -7,6 +7,15 @@ export default class Loader {
     this.entitiesRepo = new EntitiesRepo();
   }
 
+  loadSchema() {
+    return this.client
+      .get('/site', { include: 'item_types,item_types.fields' })
+      .then(site => {
+        this.siteId = site.data.id;
+        this.entitiesRepo.upsertEntities(site);
+      });
+  }
+
   load() {
     return Promise.all([
       this.client.get('/site', { include: 'item_types,item_types.fields' }),
@@ -22,6 +31,66 @@ export default class Loader {
       this.siteId = site.data.id;
       this.entitiesRepo.upsertEntities(site, items, uploads);
     });
+  }
+
+  async watchSchema(notifier) {
+    if (!this.siteId) {
+      await this.loadSchema();
+    }
+
+    const [watcher, disconnect] = await this.client.subscribeToChannel(
+      this.siteId,
+    );
+
+    const addEventListener = (eventName, entitiesRepoRefresher) => {
+      watcher.bind(eventName, data => {
+        notifier(entitiesRepoRefresher(data));
+      });
+    };
+
+    const itemVersion = this.previewMode ? 'latest' : 'published';
+
+    addEventListener('site:upsert', async () => {
+      const payloads = await Promise.all([
+        this.client.get('/site', { include: 'item_types,item_types.fields' }),
+        this.client.items.all(
+          { version: itemVersion },
+          { deserializeResponse: false, allPages: true },
+        ),
+        this.client.uploads.all(
+          {},
+          { deserializeResponse: false, allPages: true },
+        ),
+      ]);
+
+      this.entitiesRepo.upsertEntities(...payloads);
+    });
+
+    addEventListener('upload:destroy', ({ ids }) => {
+      this.entitiesRepo.destroyEntities('upload', ids);
+    });
+
+    addEventListener('item_type:upsert', async ({ ids }) => {
+      for (const id of ids) {
+        const payloads = await Promise.all([
+          this.client.itemTypes.find(id, {}, { deserializeResponse: false }),
+          this.client.items.all(
+            { 'filter[type]': id, version: itemVersion },
+            { deserializeResponse: false, allPages: true },
+          ),
+        ]);
+
+        this.entitiesRepo.upsertEntities(...payloads);
+      }
+    });
+
+    addEventListener('item_type:destroy', ({ ids }) => {
+      ids.forEach(id => {
+        this.entitiesRepo.destroyItemType(id);
+      });
+    });
+
+    return disconnect;
   }
 
   async watch(notifier) {
