3844dfe0eb29bb4d3d1e9d9044b71d1344bf604e
Irene Oppo <i.oppo@datocms.com>
2020-03-11 17:59:24 +0100

    Fix fallback value for localized fields bug for gatsby-source
    eslint

--- src/utils/localizedRead.js
+++ src/utils/localizedRead.js
@@ -3,7 +3,19 @@ export default function localizedRead(entity, key, localized, i18n) {
   if (localized && hash) {
     const fallbacks = i18n.fallbacks || {};
     const locales = [i18n.locale].concat(fallbacks[i18n.locale] || []);
-    const localeWithValue = locales.find(locale => hash[locale]);
+    const localeWithValue = locales.find(locale => {
+      if (hash[locale]) {
+        if (Array.isArray(hash[locale])) {
+          return hash[locale][0];
+        }
+        if (typeof hash[locale] === 'object') {
+          return Object.keys(hash[locale])[0];
+        }
+        return hash[locale];
+      }
+
+      return undefined;
+    });
     return localeWithValue ? hash[localeWithValue] : null;
   }
 
