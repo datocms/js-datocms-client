b0f36fe46a27e61ac34b43ffc1bc644aa0add7fa
Stefano Verna <s.verna@datocms.com>
2021-03-10 14:42:07 +0100

    Empty string fallbacks

--- src/utils/localizedRead.js
+++ src/utils/localizedRead.js
@@ -5,7 +5,9 @@ export default function localizedRead(entity, key, localized, i18n) {
     const locales = [i18n.locale].concat(fallbacks[i18n.locale] || []);
     const localeWithValue = locales.find(locale => {
       const localeValue = hash[locale];
-      return localeValue !== null && localeValue !== undefined;
+      return (
+        localeValue !== null && localeValue !== undefined && localeValue !== ''
+      );
     });
     return localeWithValue ? hash[localeWithValue] : null;
   }
