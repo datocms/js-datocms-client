cd01a320d0908db9e4e0dddef7a32767dc42bec3
Irene Oppo <i.oppo@datocms.com>
2021-05-03 13:02:47 +0200

    Add test for professional plans

---
+++ test/src/SiteClientPro_test.js
@@ -0,0 +1,65 @@
+import { expect } from 'chai';
+/* global generateNewAccountClient:true */
+
+import { SiteClient } from '../../src/index';
+
+const shouldRunTest = () => {
+  // This test is only available in developmentMode
+  // To make it pass change the CHARGEBEE_BASE_PLAN_ID to 100 in the API .env
+
+  if (
+    process.env.ACCOUNT_API_BASE_URL !== 'http://account-api.lvh.me:3001' ||
+    process.env.SITE_API_BASE_URL !== 'http://site-api.lvh.me:3001'
+  ) {
+    console.log(
+      'Pro plans test skipped. To enable it, run `npm run dev:test` and change CHARGEBEE_BASE_PLAN_ID in the API .env and ',
+    );
+
+    return false;
+  }
+
+  return true;
+};
+
+(shouldRunTest() ? describe : describe.skip)('Site API Pro plan', () => {
+  let site;
+  let client;
+
+  beforeEach(
+    vcr('before', async () => {
+      const accountClient = await generateNewAccountClient();
+      site = await accountClient.sites.create({ name: 'Blog' });
+
+      client = new SiteClient(
+        site.readwriteToken,
+        null,
+        process.env.SITE_API_BASE_URL,
+      );
+    }),
+  );
+
+  describe('sso settings', () => {
+    it(
+      'find, generateToken, update',
+      vcr(async () => {
+        const ssoSettings = await client.ssoSettings.find();
+
+        expect(ssoSettings).not.to.equal(null);
+
+        const { scimApiToken } = await client.ssoSettings.generateToken();
+
+        expect(scimApiToken).not.to.equal(ssoSettings.scimApiToken);
+
+        const updatedSsoSettings = await client.ssoSettings.update({
+          defaultRole: null,
+          idpSamlMetadataUrl:
+            'https://my-org.oktapreview.com/app/XXXX/sso/saml/metadata',
+        });
+
+        expect(updatedSsoSettings.idpSamlMetadataUrl).to.equal(
+          'https://my-org.oktapreview.com/app/XXXX/sso/saml/metadata',
+        );
+      }),
+    );
+  });
+});
